f7dc5aaef2f9166c7976bce6a19e53857d279ac1
Jiayi Liu <jiayeeliu@gmail.com>
2024-07-28 14:48:43 -0700

    metric ID refinement
    Co-authored-by: Ivan Tam <hit@sfu.ca>

--- metrics/id.py
+++ metrics/id.py
@@ -2,52 +2,44 @@ import os
 import sys
 import torch
 import numpy as np
-import open3d as o3d
+import trimesh
+from copy import deepcopy
 from pytorch3d.structures import Meshes
-from pytorch3d.io import load_ply
 from pytorch3d.ops import sample_points_from_meshes
 from pytorch3d.loss import chamfer_distance
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from objects.motions import transform_all_parts
 
-def _load_and_combine_plys(dir, ply_files, delete_temp_files=False, 
-                           temp_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), "temp")):
+def _load_and_combine_plys(dir, ply_files, scale=None, z_rotate=None, translate=None):
     """
     Load and combine the ply files into one PyTorch3D mesh
 
     - dir: the directory of the object in which the ply files are from\n
     - ply_files: the list of ply files\n
-    - delete_temp_files (optional): whether to delete the temp file after loading the mesh\n
-    - temp_dir (optional): the directory to store the combined mesh file\n
+    - scale: the scale factor to apply to the vertices\n
+    - z_rotate: whether to rotate the object around the z-axis by 90 degrees\n
+    - translate: the translation to apply to the vertices\n
 
     Return:\n
     - mesh: one PyTorch3D mesh of the combined ply files
     """
-    
-    # Create temp directory for storing the combined mesh file and delete it afterwards
-    os.makedirs(temp_dir, exist_ok=True)
 
     # Combine the ply files into one
-    full_part_mesh = o3d.geometry.TriangleMesh()
+    meshes = []
     for ply_file in ply_files:
-        mesh = o3d.io.read_triangle_mesh(os.path.join(dir, ply_file))
-        full_part_mesh += mesh
-    
-    # Save the combined mesh file
-    temp_file_path = os.path.join(temp_dir, "temp.ply")
-    o3d.io.write_triangle_mesh(temp_file_path, full_part_mesh)
+        meshes.append(trimesh.load(os.path.join(dir, ply_file), force="mesh"))
+    full_part_mesh = trimesh.util.concatenate(meshes)
 
-    # Load the combined mesh file as PyTorch3D mesh
-    vertices, faces = load_ply(temp_file_path)
-
-    # Delete the temp file if specified
-    if delete_temp_files:
-        os.remove(temp_file_path)
+    # Apply the transformations
+    full_part_mesh.vertices -= full_part_mesh.bounding_box.centroid
+    transformation = trimesh.transformations.compose_matrix(scale=scale, angles=[0, 0, np.radians(90) if z_rotate else 0], translate=translate)
+    full_part_mesh.apply_transform(transformation)
     
     # Create the PyTorch3D mesh
-    mesh = Meshes(verts=[vertices], faces=[faces])
+    mesh = Meshes(verts=torch.as_tensor(full_part_mesh.vertices, dtype=torch.float32).unsqueeze(0), 
+                  faces=torch.as_tensor(full_part_mesh.faces, dtype=torch.int64).unsqueeze(0))
 
-    return mesh    
+    return mesh
 
 def _compute_chamfer_distance(obj1_part_points, obj2_part_points):
     """
@@ -106,17 +98,28 @@ def ID(gen_obj_dict, gen_obj_retrieved_parts, gt_obj_dict, gt_obj_path, num_stat
     # Load the ply files of the two objects and sample points from them
     gen_obj_part_points = torch.zeros((gen_obj_num_parts, num_samples, 3))
     for i in range(gen_obj_num_parts):
-        part_mesh = _load_and_combine_plys(gen_obj_part_ply_paths[i]["dir"], gen_obj_part_ply_paths[i]["files"])
+        part_mesh = _load_and_combine_plys(gen_obj_part_ply_paths[i]["dir"], gen_obj_part_ply_paths[i]["files"], 
+                                           scale=gen_obj_retrieved_parts[i]["scale_factor"], 
+                                           z_rotate=gen_obj_retrieved_parts[i]["z_rotate_90"], 
+                                           translate=gen_obj_dict["diffuse_tree"][i]["aabb"]["center"])
         gen_obj_part_points[i] = sample_points_from_meshes(part_mesh, num_samples=num_samples).squeeze(0)
 
     gt_obj_part_points = torch.zeros((gt_obj_num_parts, num_samples, 3))
     for i in range(gt_obj_num_parts):
-        part_mesh = _load_and_combine_plys(gt_obj_part_ply_paths[i]["dir"], gt_obj_part_ply_paths[i]["files"])
+        part_mesh = _load_and_combine_plys(gt_obj_part_ply_paths[i]["dir"], gt_obj_part_ply_paths[i]["files"], 
+                                           translate=gt_obj_dict["diffuse_tree"][i]["aabb"]["center"])
         gt_obj_part_points[i] = sample_points_from_meshes(part_mesh, num_samples=num_samples).squeeze(0)
+    
+    original_gen_obj_part_points = deepcopy(gen_obj_part_points)
+    original_gt_obj_part_points = deepcopy(gt_obj_part_points)
 
     chamfer_distances = np.zeros(num_states)
     joint_states = np.linspace(0, 1, num_states)
     for state_idx, state in enumerate(joint_states):
+
+        # Reset the part point clouds
+        gen_obj_part_points = deepcopy(original_gen_obj_part_points)
+        gt_obj_part_points = deepcopy(original_gt_obj_part_points)
 
         # Transform the part point clouds to the current state using the joints
         transform_all_parts(gen_obj_part_points.numpy(), gen_obj_dict, state, dry_run=False)
